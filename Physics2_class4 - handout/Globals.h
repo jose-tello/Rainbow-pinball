e6df4d12bdb720e374714b239775e0e5b800d96b
adriaserrano97 <adria.serrano.97@gmail.com>
2019-11-02 10:39:17 +0100

    Defined proper SCREEN_WIDTH

--- Physics2_class4 - handout/Globals.h
+++ Physics2_class4 - handout/Globals.h
@@ -21,7 +21,7 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 662
+#define SCREEN_WIDTH 622
 #define SCREEN_HEIGHT 936
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
